a23c146738c2a3eea9568faf8fdf3c38e73e8456
Moises <moiamdev@gmail.com>
2024-05-22 14:39:21 +0200

    r

--- src/gui/principal/Login.java
+++ src/gui/principal/Login.java
@@ -5,8 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
